8282ea009ba7a39961326c785388f4b8a1a50b96
snjl <349245467@qq.com>
2019-01-18 20:11:04 +0800

    提交

---
+++ item4.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
+sender = '349245467@qq.com'
+password = 'omyfujifauhqbhaf'
+receivers = ['349245467@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+msgRoot = MIMEMultipart('related')
+msgRoot['From'] = Header("菜鸟教程", 'utf-8')
+msgRoot['To'] = Header("测试", 'utf-8')
+subject = 'Python SMTP 邮件测试'
+msgRoot['Subject'] = Header(subject, 'utf-8')
+
+msgAlternative = MIMEMultipart('alternative')
+msgRoot.attach(msgAlternative)
+
+mail_msg = """
+<p>Python 邮件发送测试...</p>
+<p><a href="">链接</a></p>
+<p>图片演示：</p>
+<p><img src="cid:image1"></p>
+"""
+msgAlternative.attach(MIMEText(mail_msg, 'html', 'utf-8'))
+
+# 指定图片为当前目录
+fp = open('a.jpg', 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+
+# 定义图片 ID，在 HTML 文本中引用
+msgImage.add_header('Content-ID', '<image1>')
+msgRoot.attach(msgImage)
+
+try:
+    server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    server.login(sender, password)
+    server.sendmail(sender, receivers, msgRoot.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException:
+    print("Error: 无法发送邮件")
---
+++ item5.py
@@ -0,0 +1,49 @@
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import parseaddr, formataddr
+
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+sender = ''
+password = ''
+receivers = ['']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+msgRoot = MIMEMultipart('related')
+msgRoot['From'] = _format_addr('Python爱好者 <%s>' % sender)
+msgRoot['To'] = _format_addr('管理员 <%s>' % password)
+msgRoot['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+
+msgAlternative = MIMEMultipart('alternative')
+msgRoot.attach(msgAlternative)
+
+mail_msg = """
+<p>Python 邮件发送测试...</p>
+<p><a href="">链接</a></p>
+<p>图片演示：</p>
+<p><img src="cid:image1"></p>
+"""
+msgAlternative.attach(MIMEText(mail_msg, 'html', 'utf-8'))
+
+# 指定图片为当前目录
+fp = open('a.jpg', 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+
+# 定义图片 ID，在 HTML 文本中引用
+msgImage.add_header('Content-ID', '<image1>')
+msgRoot.attach(msgImage)
+
+try:
+    server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    server.login(sender, password)
+    server.sendmail(sender, receivers, msgRoot.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException:
+    print("Error: 无法发送邮件")
